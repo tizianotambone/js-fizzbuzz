aed91128cec3a1272618bad40625c0e7ccf0bc1c
tizianotambone <tambonetiziano@gmail.com>
2025-03-18 15:37:56 +0100

    [UPD] exstart

---
+++ js/sript.js
@@ -0,0 +1,14 @@
+// inizio esercizio
+// VARIABILI
+let i = 1
+
+for(let i= 1; i <=100; i++){
+    if(i % 3 == 0){
+    console.log("fizz")
+        
+    }
+    else if(i % 5 == 0){
+
+        console.log("buzz")
+    }
+}
